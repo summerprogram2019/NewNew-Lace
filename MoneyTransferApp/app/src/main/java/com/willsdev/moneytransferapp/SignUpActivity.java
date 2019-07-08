b82783a6a53f371b8a53ef1a23be157e45e7b393
cav72 <51986748+cav72@users.noreply.github.com>
2019-07-08 18:40:38 +1000

    commit login update 4:40pm

--- MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/SignUpActivity.java
+++ MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/SignUpActivity.java
@@ -2,8 +2,10 @@ package com.willsdev.moneytransferapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -39,5 +41,10 @@ public class SignUpActivity extends AppCompatActivity
         NetworkThread networkThread = new NetworkThread(runQuery, this);
         networkThread.execute();
 
+        // Fresh Dropdown
+        Spinner dropdown = findViewById(R.id.country_spinner);
+        String[] items = new String[]{"Australia", "Russia", "China"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
