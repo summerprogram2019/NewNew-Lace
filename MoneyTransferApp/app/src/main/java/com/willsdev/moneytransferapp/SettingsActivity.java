a788c02f1336f99c6bfff2eca57af53b4a1fb348
Michael Fitzmaurice <michael@ezzydoc.com>
2019-07-12 11:08:03 +0800

    final commit (for now mwahahah)

--- MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/SettingsActivity.java
+++ MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.willsdev.moneytransferapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,5 +23,17 @@ public class SettingsActivity extends AppCompatActivity
         RunQuery query = new RunQuery(null,QueryType.SETTINGS,data);
         NetworkThread networkThread = new NetworkThread(query,this);
         networkThread.execute();
+
+        Button logout = findViewById(R.id.btn_logout);
+        logout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getSharedPreferences("userdetails",MODE_PRIVATE).edit().clear().apply();
+                Intent intent = new Intent(SettingsActivity.this,SplashScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
